e6fed9551191a56c0a7911e99f9a0b038c3a353f
Андрей <desmoond@outlook.com>
2019-03-24 03:00:39 +0700

    Шеньшин. Отправления событий изменения карты, если IsHide у карты был изменен. (Для того, чтобы шаблон карты обновился).

--- RPoker/ViewModel/PlayerViewModel.cs
+++ RPoker/ViewModel/PlayerViewModel.cs
@@ -11,6 +11,17 @@ namespace ViewModel
 		public PlayerViewModel()
 		{
 			PlayerState = new PlayerState();
+
+
+			this.WhenAnyValue(x => x.PlayerState.FirstCard.IsHide).Subscribe(x =>
+			{
+				PlayerState.RaisePropertyChanged(nameof(PlayerState.FirstCard));
+			});
+
+			this.WhenAnyValue(x => x.PlayerState.SecondCard.IsHide).Subscribe(x =>
+			{
+				PlayerState.RaisePropertyChanged(nameof(PlayerState.SecondCard));
+			});
 		}
 
 		#region Private properties
