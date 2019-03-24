3d9e653de9add1e631dae220276913509ca0acc9
Андрей <desmoond@outlook.com>
2019-03-25 00:07:36 +0700

    Шеньшин. Изменены интерфейсы CombinationChecker и CombinationComparer.

--- RPoker/Model/Cards/CombinationChecker.cs
+++ RPoker/Model/Cards/CombinationChecker.cs
@@ -12,7 +12,7 @@ namespace Model.Cards
 		TwoPair = 3,
 		ThreeOfKind = 4,
 		Straight = 5,
-        Flush = 6,
+    Flush = 6,
 		FullHouse = 7,
 		Kape = 8,
 		StraightFlush = 9,
@@ -21,57 +21,59 @@ namespace Model.Cards
 
 	public static class CombinationChecker
 	{
-		public static bool IsRoyalFlush(IEnumerable<Card> cards)
+		public static bool IsRoyalFlush((IEnumerable<Card>, IEnumerable<Card>) x)
+		{
+			var playerCards = x.Item1;
+			var tableCards = x.Item2;
+			return false;
+		}
+
+		public static bool IsStraightFlush((IEnumerable<Card>, IEnumerable<Card>) x)
 		{
 			return false;
 		}
 
-		public static bool IsStraightFlush(IEnumerable<Card> cards)
+		public static bool IsKape((IEnumerable<Card>, IEnumerable<Card>) x)
 		{
 			return false;
 		}
 
-		public static bool IsKape(IEnumerable<Card> cards)
+		public static bool IsFullHouse((IEnumerable<Card>, IEnumerable<Card>) x)
 		{
 			return false;
 		}
 
-		public static bool IsFullHouse(IEnumerable<Card> cards)
+		public static bool IsFlush((IEnumerable<Card>, IEnumerable<Card>) x)
 		{
 			return false;
 		}
 
-		public static bool IsFlush(IEnumerable<Card> cards)
+		public static bool IsStraight((IEnumerable<Card>, IEnumerable<Card>) x)
 		{
 			return false;
 		}
 
-		public static bool IsStraight(IEnumerable<Card> cards)
+		public static bool IsThreeOfKind((IEnumerable<Card>, IEnumerable<Card>) x)
 		{
 			return false;
 		}
 
-		public static bool IsThreeOfKind(IEnumerable<Card> cards)
+		public static bool IsTwoPair((IEnumerable<Card>, IEnumerable<Card>) x)
 		{
 			return false;
 		}
 
-		public static bool IsTwoPair(IEnumerable<Card> cards)
+		public static bool IsPair((IEnumerable<Card>, IEnumerable<Card>) x)
 		{
 			return false;
 		}
 
-		public static bool IsPair(IEnumerable<Card> cards)
+		public static bool IsHightCard((IEnumerable<Card>, IEnumerable<Card>) x)
 		{
 			return false;
 		}
 
-		public static bool IsHightCard(IEnumerable<Card> cards)
-		{
-			return false;
-		}
-
-		public static Combination DetermineCombination(IEnumerable<Card> cards)
+		public static Combination DetermineCombination((IEnumerable<Card>, IEnumerable<Card>) x)
 		{
 			return Combination.FullHouse;
 		}
--- RPoker/Model/Cards/CombinationComparer.cs
+++ RPoker/Model/Cards/CombinationComparer.cs
@@ -5,23 +5,13 @@ using System.Linq;
 
 namespace Model.Cards
 {
-	public class CombinationComparer : IComparer<IEnumerable<Card>>
+	public class CombinationComparer : IComparer<(IEnumerable<Card>, IEnumerable<Card>)>
 	{
-		public int Compare(IEnumerable<Card> x, IEnumerable<Card> y)
+		public int Compare((IEnumerable<Card>, IEnumerable<Card>) x, (IEnumerable<Card>, IEnumerable<Card>) y)
 		{
-            var xComb = CombinationChecker.DetermineCombination(x);
-            var yComb = CombinationChecker.DetermineCombination(y);
-            if (xComb != yComb)
-                return (int) xComb - (int) yComb;
-
-            var xList = x.ToList();
-            var yList = y.ToList();
-            CardComparer comparator = new CardComparer();
-            xList.Sort(comparator);
-            yList.Sort(comparator);
-
-            return comparator.Compare(xList[0], yList[0]);
-        }
+			var playerCards = x.Item1;
+			var tableCards = x.Item2;
+		}
 
 	}
 }
