a47070475ebb85bc3c4c7b7b3156124f7b2c0ff8
kremer.nikol <kremer.nikol@mail.ru>
2019-03-24 23:11:43 +0700

    Кремер. Добавлен метод CombinationComparer::Compare.

--- RPoker/Model/Cards/CombinationComparer.cs
+++ RPoker/Model/Cards/CombinationComparer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace Model.Cards
 {
@@ -8,7 +9,19 @@ namespace Model.Cards
 	{
 		public int Compare(IEnumerable<Card> x, IEnumerable<Card> y)
 		{
-			throw new NotImplementedException();
-		}
+            var xComb = CombinationChecker.DetermineCombination(x);
+            var yComb = CombinationChecker.DetermineCombination(y);
+            if (xComb != yComb)
+                return (int) xComb - (int) yComb;
+
+            var xList = x.ToList();
+            var yList = y.ToList();
+            CardComparer comparator = new CardComparer();
+            xList.Sort(comparator);
+            yList.Sort(comparator);
+
+            return comparator.Compare(xList[0], yList[0]);
+        }
+
 	}
 }
